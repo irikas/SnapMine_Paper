9382aa90e9089ecbbf04749fa1b2adf6bfa1b695
irikas <sinhairika@gmail.com>
2025-12-04 11:51:44 -0800

    Update extract_counts.R

--- snapmine/R/extract_counts.R
+++ snapmine/R/extract_counts.R
@@ -101,10 +101,9 @@ snaptronQuery <- function(uniqueCanonJunc) {
     # Estimated remaining time based on the mean time that took to run the previous iterations
     est <- nrow(uniqueCanonJunc) * (mean(end[end != 0] - init[init != 0])) - time
     est <- round(seconds_to_period(est), 0)
-    cat(paste0(" // ", i, " of ", nrow(uniqueCanonJunc), " // ETR: ", est), "")
     cat(paste(
       " // ", i, " of ", nrow(uniqueCanonJunc),
-      " // ETR:", remainining
+      " // ETR:", est
     ), "")
 
     rm(i)
